0fda9da42e5d6a50be73faa3be7883bd94979b48
Thefolle <s280174@studenti.polito.it>
2022-03-30 18:54:49 +0200

    Correct hateoas link on home controller

--- songs-salad-main/src/main/kotlin/com/thefolle/controller/HomeController.kt
+++ songs-salad-main/src/main/kotlin/com/thefolle/controller/HomeController.kt
@@ -1,24 +1,23 @@
 package com.thefolle.controller
 
 import org.springframework.hateoas.CollectionModel
-import org.springframework.hateoas.EntityModel
-import org.springframework.hateoas.Link
-import org.springframework.hateoas.LinkRelation
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import java.util.*
 
 @RestController
 class HomeController {
 
     @GetMapping("")
     fun home(): ResponseEntity<CollectionModel<Set<*>>> {
+        val link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(SongController::class.java).getSongs(null, null)).withRel("songs")
+
         return ResponseEntity
                 .ok(
                         CollectionModel
                                 .empty(
-                                        Link.of("http://localhost:8081/songs", LinkRelation.of("songs"))
+                                        link
                                 )
 
                 )
